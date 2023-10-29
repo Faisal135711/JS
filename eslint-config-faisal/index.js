b3c57727a46afcafd9b48924a8f62ae9986ebca0
Faisal135711 <alpha316mnr@gmail.com>
2023-10-29 10:31:40 +0600

    Fix issue

---
+++ eslint-config-faisal/index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  rules: {
+    "eqeqeq": "error",
+    "no-unused-vars": "off",
+    "no-restricted-imports": ["error", "underscore"],
+    "no-console": "off",
+    "consistent-return": "error",
+    "indent": [
+        "error",
+        4
+    ],
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    "quotes": [
+        "error",
+        "single"
+    ],
+    "semi": [
+        "error",
+        "always"
+    ]
+  }
+}
