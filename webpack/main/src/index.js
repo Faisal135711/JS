fe4cba7547eb4dba628c5f474ae383d074bbde58
Faisal135711 <alpha316mnr@gmail.com>
2023-11-06 22:57:09 +0600

    Add webpack files

---
+++ webpack/main/src/index.js
@@ -0,0 +1,8 @@
+import generateJoke from "./generateJoke";
+import "./styles/main.scss";
+import image from "./assets/image.jpg";
+
+document.getElementById("img").src = image;
+
+console.log(generateJoke());
+console.log(123);
